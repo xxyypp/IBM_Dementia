cd26197174a152bc8b621d0121cc28a9aae1dd58
unknown <yangpuypyp@outlook.com>
2018-05-24 16:26:57 +0100

    User setting done

--- MyApp/app/src/main/java/com/example/shreyus/myapp/UserSetting.java
+++ MyApp/app/src/main/java/com/example/shreyus/myapp/UserSetting.java
@@ -23,23 +23,26 @@ public class UserSetting extends AppCompatActivity {
         setContentView(R.layout.activity_user_setting);
 
         Intent intent = getIntent();
-        String num1 = intent.getStringExtra(MainActivity.person1_id);
-        String num2 = intent.getStringExtra(MainActivity.person2_id);
-        String num3 = intent.getStringExtra(MainActivity.person3_id);
+//        String num1 = intent.getStringExtra(MainActivity.person1_id);
+//        String num2 = intent.getStringExtra(MainActivity.person2_id);
+//        String num3 = intent.getStringExtra(MainActivity.person3_id);
 
         /******** Display info *********/
         EditText editNum1 = findViewById(R.id.editNum1);
         EditText editNum2 = findViewById(R.id.editNum2);
         EditText editNum3 = findViewById(R.id.editNum3);
 
+        SharedPreferences dataSaved = getSharedPreferences(PREFS_NAME, 0);
+        String num1 = dataSaved.getString(person1_id,null);
+        String num2 = dataSaved.getString(person2_id,null);
+        String num3 = dataSaved.getString(person3_id,null);
+        //EditText edittest = findViewById(R.id.test);
+
         editNum1.setText(num1);
         editNum2.setText(num2);
         editNum3.setText(num3);
 
-        SharedPreferences dataSaved = getSharedPreferences(PREFS_NAME, 0);
-        String test = dataSaved.getString(person1_id,null);
-        EditText edittest = findViewById(R.id.test);
-        edittest.setText(test);
+
 
     }
 
@@ -55,9 +58,17 @@ public class UserSetting extends AppCompatActivity {
         String num2 = editNum2.getText().toString();
         String num3 = editNum3.getText().toString();
 
-        tomainIntent.putExtra(person1_id, num1);
-        tomainIntent.putExtra(person2_id, num2);
-        tomainIntent.putExtra(person3_id, num3);
+        //saved to file
+        SharedPreferences savedFile = getSharedPreferences(PREFS_NAME,0);
+        SharedPreferences.Editor editor = savedFile.edit();
+        editor.putString(person1_id, num1);
+        editor.putString(person2_id,num2);
+        editor.putString(person3_id,num3);
+        editor.commit();
+
+//        tomainIntent.putExtra(person1_id, num1);
+//        tomainIntent.putExtra(person2_id, num2);
+//        tomainIntent.putExtra(person3_id, num3);
 
         setResult(RESULT_UPDATE,tomainIntent);
         finish();
