d2474fddb56eebd0f1b20a6152b9d683691ec819
unknown <yangpuypyp@outlook.com>
2018-06-06 14:30:38 +0100

    Todo list done

--- MyApp/app/src/main/java/com/example/shreyus/myapp/MainActivity.java
+++ MyApp/app/src/main/java/com/example/shreyus/myapp/MainActivity.java
@@ -19,14 +19,19 @@ import java.util.ArrayList;
 import android.Manifest;
 import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.app.ProgressDialog;
 import android.app.TaskStackBuilder;
+import android.content.ContentValues;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.drawable.BitmapDrawable;
@@ -43,6 +48,7 @@ import android.telephony.SmsManager;
 import android.support.v4.content.ContextCompat;
 import android.util.Base64;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.util.Log;
 import android.widget.AdapterView;
@@ -119,12 +125,17 @@ class Locations {
     }
 }
 
-
-public class MainActivity extends FragmentActivity implements GoogleMap.OnMyLocationButtonClickListener,
+//FragmentActivity
+public class MainActivity extends AppCompatActivity implements GoogleMap.OnMyLocationButtonClickListener,
         GoogleMap.OnMyLocationClickListener, OnMapReadyCallback {
 
     /********************************************** Pre-define ******************************************/
 
+    /******** To-Do list *********/
+    private static final String TAG = "MainActivity";
+    private TaskDbHelper mHelper;
+    private ListView mTaskListView;
+    private ArrayAdapter<String> mAdapter;
 
     //User Setting
     //public static final String EXTRA_MSG="com.example.myapp.usersetting";
@@ -199,6 +210,7 @@ public class MainActivity extends FragmentActivity implements GoogleMap.OnMyLoca
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
         //retrieve data saved
         SharedPreferences dataSaved = getSharedPreferences(PREFS_NAME, 0);
@@ -212,7 +224,12 @@ public class MainActivity extends FragmentActivity implements GoogleMap.OnMyLoca
             editor.commit();
         }
 
-        setContentView(R.layout.activity_main);
+        //To-do list
+        mHelper = new TaskDbHelper(this);
+        mTaskListView = findViewById(R.id.list_todo);
+        updateUI();
+
+
 
 
         //sendNotification();
@@ -355,6 +372,86 @@ public class MainActivity extends FragmentActivity implements GoogleMap.OnMyLoca
             }
         });
 
+    }
+
+    /******* To-Do List *************/
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_add_task:
+                final EditText taskEditText = new EditText(this);
+                AlertDialog dialog = new AlertDialog.Builder(this)
+                        .setTitle("Add a new task")
+                        .setMessage("What do you want to do next?")
+                        .setView(taskEditText)
+                        .setPositiveButton("Add", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                String task = String.valueOf(taskEditText.getText());
+                                SQLiteDatabase db = mHelper.getWritableDatabase();
+                                ContentValues values = new ContentValues();
+                                values.put(TaskContract.TaskEntry.COL_TASK_TITLE, task);
+                                db.insertWithOnConflict(TaskContract.TaskEntry.TABLE,
+                                        null,
+                                        values,
+                                        SQLiteDatabase.CONFLICT_REPLACE);
+                                db.close();
+                                updateUI();
+                            }
+                        })
+                        .setNegativeButton("Cancel", null)
+                        .create();
+                dialog.show();
+                return true;
+            case R.id.user_setting:
+                openUserSettings();
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void updateUI() {
+        ArrayList<String> taskList = new ArrayList<>();
+        SQLiteDatabase db = mHelper.getReadableDatabase();
+        Cursor cursor = db.query(TaskContract.TaskEntry.TABLE,
+                new String[]{TaskContract.TaskEntry._ID, TaskContract.TaskEntry.COL_TASK_TITLE},
+                null, null, null, null, null);
+        while (cursor.moveToNext()) {
+            int idx = cursor.getColumnIndex(TaskContract.TaskEntry.COL_TASK_TITLE);
+            taskList.add(cursor.getString(idx));
+        }
+
+        if (mAdapter == null) {
+            mAdapter = new ArrayAdapter<>(this,
+                    R.layout.item_todo,
+                    R.id.task_title,
+                    taskList);
+            mTaskListView.setAdapter(mAdapter);
+        } else {
+            mAdapter.clear();
+            mAdapter.addAll(taskList);
+            mAdapter.notifyDataSetChanged();
+        }
+
+        cursor.close();
+        db.close();
+    }
+    public void deleteTask(View view) {
+        View parent = (View) view.getParent();
+        TextView taskTextView = (TextView) parent.findViewById(R.id.task_title);
+        String task = String.valueOf(taskTextView.getText());
+        SQLiteDatabase db = mHelper.getWritableDatabase();
+        db.delete(TaskContract.TaskEntry.TABLE,
+                TaskContract.TaskEntry.COL_TASK_TITLE + " = ?",
+                new String[]{task});
+        db.close();
+        updateUI();
     }
     /********************************************** Implementation: User setting ******************************************/
     void UpdateBooleanUserSetting(SharedPreferences dataSaved){
